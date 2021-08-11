2e250e0aa2ee8591b931486b614eb28969898246
Tino <Tinotenda.Kanyandura@standardbank.co.za>
2021-08-11 05:24:22 +0200

    Adding beer repository. closes #6

---
+++ src/main/java/com/tinotenda/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.tinotenda.msscbeerservice.repositories;
+
+import com.tinotenda.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
