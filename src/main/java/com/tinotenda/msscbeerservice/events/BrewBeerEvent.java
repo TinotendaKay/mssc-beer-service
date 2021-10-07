d5019caa0199ee7e20e9b8fdf6eb59acc6be9d35
Tino <Tinotenda.Kanyandura@standardbank.co.za>
2021-10-07 18:12:33 +0200

    Show debug level output.

--- src/main/java/com/tinotenda/msscbeerservice/events/BrewBeerEvent.java
+++ src/main/java/com/tinotenda/msscbeerservice/events/BrewBeerEvent.java
@@ -1,11 +1,9 @@
 package com.tinotenda.msscbeerservice.events;
 
 import com.tinotenda.msscbeerservice.web.model.BeerDto;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 public class BrewBeerEvent extends BeerEvent {
 
     public BrewBeerEvent(BeerDto beerDto) {
